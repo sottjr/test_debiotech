c416bd8592ec6160bdc3a6c3a3083d92339d4f6e
Junior Sott <sottjr@gmail.com>
2023-02-21 00:05:17 +0100

    chore: rename itens for send files

--- index.js
+++ index.js
@@ -1,49 +1,91 @@
-let valueExibition = [];
+const valueExibition = [];
 let timer = 0;
+let intervalId;
 
 function addTaskA() {
-  valueExibition.push("A")
+    valueExibition.push("A");
 }
 
 function addTaskB() {
-  valueExibition.push("B")
+    valueExibition.push("B");
 }
 
 function addTaskC() {
-  valueExibition.push("C")
+    valueExibition.push("C");
 }
 
 function addTaskD() {
-  valueExibition.push("D")
+    valueExibition.push("D");
 }
 
 function addTaskE() {
-  valueExibition.push("E")
+    valueExibition.push("E");
 }
 
 function clearTasks() {
-  valueExibition = [];
+    valueExibition.length = 0;
 }
 
-setInterval(() => {
-  timer++;
-  clearTasks();
+function startTimer() {
+    intervalId = setInterval(() => {
+        timer++;
+        clearTasks();
 
-  if (timer % 1 === 0) {
-    addTaskA();
-  }
-  if (timer % 5 === 2) {
-    addTaskB();
-  }
-  if (timer % 5 === 3) {
-    addTaskC();
-  }
-  if (timer % 10 === 4) {
-    addTaskD();
-  }
-  if (timer % 10 === 5) {
-    addTaskE();
-  }
+        if (timer % 1 === 0) {
+            addTaskA();
+        }
+        if (timer % 5 === 2) {
+            addTaskB();
+        }
+        if (timer % 5 === 3) {
+            addTaskC();
+        }
+        if (timer % 10 === 4) {
+            addTaskD();
+        }
+        if (timer % 10 === 5) {
+            addTaskE();
+        }
 
-  console.log(`${timer} -> ${valueExibition.join(" ")}`);
-}, 1000);
+        const timerDisplay = document.getElementById("timerDisplay");
+        const tasksDisplay = document.getElementById("tasksDisplay");
+
+        timerDisplay.innerHTML = `Timer: ${timer} s`;
+        tasksDisplay.innerHTML = `Running tasks: ${valueExibition.join(" ")}`;
+        console.log(`${timer} -> ${valueExibition.join(" ")}`);
+
+        //information on screen when running
+
+        const content = document.querySelector('#displayContent');
+        const newElement = document.createElement("button");
+        newElement.classList.add('btnTasks');
+        newElement.textContent = `${timer} -> ${valueExibition.join(" ")}`;
+        content.appendChild(newElement);
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(intervalId);
+}
+
+function resetTimer() {
+    timer = 0;
+    clearTasks();
+
+    const timerDisplay = document.getElementById("timerDisplay");
+    const tasksDisplay = document.getElementById("tasksDisplay");
+
+    timerDisplay.innerHTML = `Timer: ${timer} s`;
+    tasksDisplay.innerHTML = `Running tasks: ${valueExibition.join(", ")}`
+
+    const meuContainer = document.getElementById('displayContent');
+    meuContainer.innerHTML = "";
+}
+
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
